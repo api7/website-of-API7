c8d9a1c0fdad9afdd8db3954fdd854ee38c2fff3
idevz <zhoujing_k49@163.com>
2019-04-09 10:56:58 +0800

    start iresty

---
+++ run.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+### BEGIN ###
+# Author: idevz
+# Since: 10:47:26 2019/04/09
+# Description:       deploy iresty.com
+# deploy          ./run.sh
+#
+# Environment variables that control this script:
+#
+### END ###
+
+set -e
+
+deploy() {
+    echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+
+    # Build the project.
+    hugo # if using a theme, replace with `hugo -t <YOURTHEME>`
+
+    # Go To Public folder
+    cd docs
+    # Add changes to git.
+    git add .
+
+    # Commit changes.
+    msg="rebuilding site $(date)"
+    if [ $# -eq 1 ]; then
+        msg="$1"
+    fi
+    git commit -m "$msg"
+
+    # Push source and build repos.
+    git push origin master
+
+    # Come Back up to the Project Root
+    cd ..
+}
+
+case "${1}" in
+deploy)
+    deploy ${2}
+    ;;
+*)
+    echo "
+Usage:
+
+	./run.sh options [arguments]
+
+The options are:
+
+    ./run.sh deploy [commit msg]             run deploy commit msg
+"
+    ;;
+esac
